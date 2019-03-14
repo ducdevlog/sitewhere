31c0ddf19d7f6909fc4e9c1437f63eba462b4f7e
Giang Viet Anh <giangvietanh@gmail.com>
2019-03-14 13:46:14 +0700

    add jar file extended grpc

--- sitewhere-java-model/src/main/java/com/sitewhere/rest/model/extended/event/request/ExtendedRequest.java
+++ sitewhere-java-model/src/main/java/com/sitewhere/rest/model/extended/event/request/ExtendedRequest.java
@@ -1,9 +1,20 @@
+/*
+ * Copyright (c) SiteWhere, LLC. All rights reserved. http://www.sitewhere.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package com.sitewhere.rest.model.extended.event.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.sitewhere.rest.model.common.MetadataProvider;
 import com.sitewhere.spi.extended.event.request.IExtendedRequest;
+
+import java.time.Instant;
+import java.util.Date;
 
 
 @JsonInclude(Include.NON_NULL)
@@ -12,6 +23,7 @@ public class ExtendedRequest extends MetadataProvider
 
 	private String requestType;
 	private Object data;
+	private Date updateDate;
 
 	@Override
 	public String getRequestType() {
@@ -23,11 +35,20 @@ public class ExtendedRequest extends MetadataProvider
 		return data;
 	}
 
+	@Override
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
 	public void setRequestType(String requestType) {
 		this.requestType = requestType;
 	}
 
 	public void setData(String data) {
 		this.data = data;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
 	}
 }
