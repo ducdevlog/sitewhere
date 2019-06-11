3b7ddab17e75e9bbca3a40804ae710736ee3c06a
Giang Viet Anh <giangvietanh@gmail.com>
2019-06-11 13:30:04 +0700

    change spring platform and add spring cloud

--- service-certificate/src/main/java/com/sitewhere/certificate/persistence/dto/IssuerData.java
+++ service-certificate/src/main/java/com/sitewhere/certificate/persistence/dto/IssuerData.java
@@ -1,5 +1,11 @@
+/*
+ * Copyright (c) SiteWhere, LLC. All rights reserved. http://www.sitewhere.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package com.sitewhere.certificate.persistence.dto;
-
 
 import org.bouncycastle.asn1.x500.X500Name;
 
