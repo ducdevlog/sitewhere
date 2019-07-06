b594163b524a1c249887b0e75827f4b33ee0fe79
Giang Viet Anh <giangvietanh@gmail.com>
2019-07-06 16:13:24 +0700

    add device event statistic

--- service-event-management/src/main/java/com/sitewhere/event/DeviceEventManagementDecorator.java
+++ service-event-management/src/main/java/com/sitewhere/event/DeviceEventManagementDecorator.java
@@ -221,6 +221,6 @@ public class DeviceEventManagementDecorator extends TenantEngineLifecycleCompone
 
     @Override
     public List<DeviceEventStatistic> getDeviceEventStaticsById(UUID token, String filterType, DateType dateType, Date startDate, Date endDate) throws SiteWhereException {
-        return null;
+        return getDelegate().getDeviceEventStaticsById(token, filterType, dateType, startDate, endDate);
     }
 }
